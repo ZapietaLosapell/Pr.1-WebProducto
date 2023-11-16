4f1b2fb9a2a052e75793ee57ded6bac636a4c080
OlgaMiguelez <146369046+OlgaMiguelez@users.noreply.github.com>
2023-11-16 13:37:36 +0100

    enlazado

--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
-let items = document.querySelectorAll('.slider.card');
-    let next = document.getElementById('next');
-    let prev = document.getElementById('prev');
-    
-    let active = 3;
-    function loadShow(){
-        let stt = 0;
-        items[active].style.transform = `none`;
-        items[active].style.zIndex = 1;
-        items[active].style.filter = 'none';
-        items[active].style.opacity = 1;
-        for(var i = active + 1; i < items.length; i++){
-            stt++;
-            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
-            items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(5px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0.6;
-        }
-        stt = 0;
-        for(var i = active - 1; i >= 0; i--){
-            stt++;
-            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
-            items[i].style.zIndex = -stt;
-            items[i].style.filter = 'blur(5px)';
-            items[i].style.opacity = stt > 2 ? 0 : 0.6;
-        }
-    }
+// jslint devel: true
+// eslint-env browser
 
-    loadShow();
+const pase2 = [...document.querySelectorAll(".card-2")];
+const pase1 = [...document.querySelectorAll(".card-1")];
+const control = document.getElementById("controlPrev");
 
-    next.onclick = function(){
-        active = active + 1 < items.length ? active + 1 : active;
-        loadShow();
+ pase1.forEach(card1 => {
+    "use strict";
+     control.addEventListener("click", () => {
+         card1.classList.toggle("backSelected");
+     });
+ });
+
+pase2.forEach(card2 => {
+    "use strict";
+    control.addEventListener("click", () => {
+        card2.classList.toggle("backSale");
+    });
+});
+
+const controls = document.getElementById("controlNext");
+
+pase1.forEach(carda => {
+    "use strict";
+     controls.addEventListener("click", () => {
+         carda.classList.toggle("sale");
+     });
+ });
+
+pase2.forEach(cardb => {
+    "use strict";
+    controls.addEventListener("click", () => {
+        cardb.classList.toggle("selected");
+    });
+});
+
+
+window.onscroll = function() {fijarMenu()};
+const header = document.getElementById("blanco");
+
+function fijarMenu() {
+    if (document.documentElement.scrollTop > 25) {
+      header.className = "blancoFijo";
+      header.firstElementChild.style.fontSize = "1em";
+      document.getElementById("mdesplegado").className = "mdesplegado";
+      document.getElementById("blancoFijoMenu").className = "blancoFijoMenu";
+    } else {
+      document.getElementById("blanco").className = "navbar";
     }
-    prev.onclick = function(){
-        active = active - 1 >= 0 ? active - 1 : active;
-        loadShow();
-    }
+  }
+
+  
+  const collapseElementList = document.querySelectorAll('.collapse')
+  const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
     
